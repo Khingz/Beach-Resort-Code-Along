7f03fc053da0869916cf3968f7b9a194d849de0f
khingz <kingsley995mr@gmail.com>
2021-02-07 05:22:39 +0100

    banner added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return(
         <>
-            Hello From Home PAge
+            <Hero>
+                <Banner title='luxurious rooms' subtitle='deluxe rooms starting from $234'>
+                    <Link to='/rooms' className='btn-primary'>
+                        our rooms
+                    </Link>
+                </Banner>
+            </Hero>
         </>  
     )
     
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+
 
 const Rooms = () => {
     return(
         <>
-            Hello From Rooms PAge
+           <Hero hero='roomsHero'>
+               <Banner title='our rooms'>
+                <Link to='/' className='btn-primary'>
+                    return home
+                </Link>
+               </Banner>
+           </Hero>
         </>   
     )
     
 }
 
 
-export default Rooms
+export default Rooms;
