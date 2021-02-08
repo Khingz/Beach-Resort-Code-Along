a4bf4a7dd6b147bd7c06dbe89b587a03120cc64b
khingz <kingsley995mr@gmail.com>
2021-02-08 10:48:03 +0100

    Featured room section completed

--- src/Context.js
+++ src/Context.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import items from './data';
 
 const RoomContext = React.createContext();
 
 class RoomProvider extends React.Component {
-    state = {};
+    state = {
+        rooms: [],
+        sortedRooms: [],
+        featuredRooms: [],
+        loading: true
+    };
+
+    //get data function
+
+    componentDidMount() {
+        let rooms = this.formatData(items)
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        this.setState({
+            loading: false,
+            featuredRooms,
+            rooms,
+            sortedRooms: rooms
+        })
+    }
+
+    formatData(items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id;
+            let images = item.fields.images.map(image => {
+                return image.fields.file.url
+            });
+            let room = {...item.fields, images, id}
+
+            return room;
+        })
+        return tempItems
+    }
+
     render() {
         return (
             <>
-                <RoomContext.Provider value={'hello'}>
+                <RoomContext.Provider value={{...this.state }}>
                     {this.props.children}
                 </RoomContext.Provider>
             </>
